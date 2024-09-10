405d013fad2b23a3123db17a7b8581d37179c3a7
shreyas <shreyaskalvankar@gmail.com>
2024-09-10 10:03:45 +0200

    remove vscoq, add keymaps to coq-lsp

--- .config/nvim/lua/shreyas/plugins/lsp/lspconfig.lua
+++ .config/nvim/lua/shreyas/plugins/lsp/lspconfig.lua
@@ -146,27 +146,5 @@ return {
         },
       },
     })
-
-    -- -- configure vscoq server
-    -- require("vscoq").setup({
-    --   vscoq = {
-    --     proof = {
-    --       -- In manual mode, don't move the cursor when stepping forward/backward a command
-    --       cursor = { sticky = true },
-    --     },
-    --   },
-    --   lsp = {
-    --     filetypes = { "coq" },
-    --     on_attach = function(client, bufnr)
-    --       -- your mappings, etc
-    --
-    --       -- In manual mode, use ctrl-alt-{j,k,l} to step.
-    --       vim.keymap.set({ "n", "i" }, "<C-M-j>", "<Cmd>VsCoq stepForward<CR>", { buffer = bufnr })
-    --       vim.keymap.set({ "n", "i" }, "<C-M-k>", "<Cmd>VsCoq stepBackward<CR>", { buffer = bufnr })
-    --       vim.keymap.set({ "n", "i" }, "<C-M-l>", "<Cmd>VsCoq interpretToPoint<CR>", { buffer = bufnr })
-    --     end,
-    --     autostart = true, -- use this if you want to manually `:LspStart vscoqtop`.
-    --   },
-    -- })
   end,
 }
