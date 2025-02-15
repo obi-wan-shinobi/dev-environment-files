1edd3e7210a14c314c75538a9de3138bc6d37f3e
Shreyas Kalvankar <shreyaskalvankar@gmail.com>
2025-02-15 19:05:09 +0100

    fix tex syntax highlighting

--- .config/nvim/lua/shreyas/plugins/nvim-treesitter.lua
+++ .config/nvim/lua/shreyas/plugins/nvim-treesitter.lua
@@ -10,9 +10,9 @@ return {
       -- A list of parser names, or "all" (the five listed parsers should always be installed)
       ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "python", "rust", "bash" },
 
-      -- Automatically install missing parsers when entering buffer
+      -- -- Automatically install missing parsers when entering buffer
       -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
-      auto_install = true,
+      auto_install = false,
 
       highlight = {
         enable = true,
